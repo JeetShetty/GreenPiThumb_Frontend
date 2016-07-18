0a736691b1132ca50537e51683b8ce21f7a8a750
Michael Lynch <mtlynch@google.com>
2016-07-18 11:07:17 -0400

    Adding graphs for all metrics
    Updating the graph directive to make it generic to support any metric
    Updating the HTML to add graphs for all metrics to the view

--- static/dashboard.js
+++ static/dashboard.js
@@ -6,10 +6,12 @@ var dashboardApp = angular.module('dashboardApp', []).
       replace: false,
       scope: {data: '=chartData'},
       link: function (scope, element, attrs) {
+        var propertyExpression = attrs['valueProperty'];
+
         // Set the dimensions of the canvas / graph
-        var margin = {top: 30, right: 20, bottom: 30, left: 50},
-          width = 600 - margin.left - margin.right,
-          height = 270 - margin.top - margin.bottom;
+        var margin = {top: 30, right: 20, bottom: 30, left: 50};
+        var width = 900 - margin.left - margin.right;
+        var height = 450 - margin.top - margin.bottom;
 
         // Parse the date / time
         var parseDate = d3.utcParse("%Y%m%dT%H:%M:%SZ");
@@ -27,12 +29,13 @@ var dashboardApp = angular.module('dashboardApp', []).
         // Define the line
         var valueline = d3.line()
           .x(function(d) { return x(d.timestamp); })
-          .y(function(d) { return y(d.temperature); });
+          .y(function(d) { return y(d.value); });
 
         var updateGraph = function(data) {
-          scope.data.forEach(function(d) {
+
+          data.forEach(function(d) {
             d.timestamp = parseDate(d.timestamp);
-            d.temperature = +d.temperature;
+            d.value = scope.$eval(propertyExpression, d);
           });
 
           // Add the svg canvas
@@ -46,7 +49,7 @@ var dashboardApp = angular.module('dashboardApp', []).
 
           // Scale the range of the data
           x.domain(d3.extent(data, function(d) { return d.timestamp; }));
-          y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
+          y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
           // Add the valueline path.
           svg.append("path")
@@ -80,16 +83,20 @@ dashboardApp.run(function($http) {
     model.temperature = temperatureHistory;
   });
   $http.get('/ambientHumidityHistory.json').success(function(humidityHistory) {
-    model.humidity = humidityHistory[humidityHistory.length - 1].humidity;
+    model.humidity = humidityHistory;
+    model.latestHumidity = humidityHistory[humidityHistory.length - 1].humidity;
   });
   $http.get('/reservoirHistory.json').success(function(reservoirHistory) {
-    model.reservoirLevel = reservoirHistory[reservoirHistory.length - 1].water_ml;
+    model.reservoirLevel = reservoirHistory;
+    model.latestReservoirLevel = reservoirHistory[reservoirHistory.length - 1].water_ml;
   });
   $http.get('/lightHistory.json').success(function(lightHistory) {
-    model.lightLevel = lightHistory[lightHistory.length - 1].light_pct;
+    model.lightLevel = lightHistory;
+    model.latestLightLevel = lightHistory[lightHistory.length - 1].light_pct;
   });
   $http.get('/soilMoistureHistory.json').success(function(moistureHistory) {
-    model.soilMoisture = moistureHistory[moistureHistory.length - 1].moisture;
+    model.soilMoisture = moistureHistory;
+    model.latestSoilMoisture = moistureHistory[moistureHistory.length - 1].moisture;
   });
 });
 
