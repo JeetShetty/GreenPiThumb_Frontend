2fe310c7c8bf82fcba0927b9ee926bc6d209a56d
Michael Lynch <mtlynch@users.noreply.github.com>
2017-02-04 14:31:18 -0500

    Removing static files (#9)
    * Removing static files
    
    We've moved these to a separate repo:
    https://github.com/JeetShetty/GreenPiThumb_Frontend_static/pull/1
    
    Also removes the static file handling from server.py since we'll handle that
    with nginx.
    
    * Removing unused import

--- server/server.py
+++ server/server.py
@@ -5,7 +5,6 @@ import argparse
 import json
 
 import klein
-from twisted.web.static import File
 
 import dummy_values
 
@@ -36,17 +35,6 @@ def main(args):
     @app.route('/ambientHumidityHistory.json')
     def ambient_humidity_history(request):
         return json.dumps(dummy_values.generate_values('humidity', 87.3, 6.0))
-
-    # TODO(mtlynch): It might make more sense to bring nginx into this stack so
-    # that nginx handles static files and Python just handles things that have
-    # to be calculated on the fly. This is okay for now, though.
-    @app.route('/dashboard')
-    def home(request):
-        return File('./static/dashboard.html')
-
-    @app.route('/static/', branch=True)
-    def static(request):
-        return File('./static')
 
     app.run('0.0.0.0', args.port)
 
