6723e97c8e8809f50ccfc94f50bbdbb7fcb517c1
Michael Lynch <mtlynch@google.com>
2017-04-01 21:19:59 -0400

    Changing RecordEncoder to generic Encoder
    RecordEncoder doesn't actually do anything record-specific and we have other
    objects (image metadata) that we want to encode with JSON. A normal JSON
    encoder can't encode datetimes, but we can just make RecordEncoder a generic
    Encoder that encodes datetimes as ISO-8601.
    
    Also makes the small change that timestamps are assumed to be in UTC time
    zone, related to the changes in the backend:
    
    * https://github.com/JeetShetty/GreenPiThumb/pull/101
    * https://github.com/JeetShetty/GreenPiThumb/pull/109
    
    Changes the serialized format to append a Z (representing UTC time zone) instead
    of an offset from UTC in hours.

--- server/json_encoder.py
+++ server/json_encoder.py
@@ -4,12 +4,12 @@ import simplejson
 
 
 def _encode_time(time):
-    """Encodes time in YYYYMMDDTHHMMSS+Z format."""
-    return datetime.datetime.strftime(time, '%Y%m%dT%H%M%S%z')
+    """Encodes time in YYYYMMDDTHHMMSSZ format (assumes UTC time zone)."""
+    return datetime.datetime.strftime(time, '%Y%m%dT%H%M%SZ')
 
 
-class RecordEncoder(simplejson.JSONEncoder):
-    """JSON encoder for GreenPiThumb DB records."""
+class Encoder(simplejson.JSONEncoder):
+    """JSON encoder for GreenPiThumb objects."""
 
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
