a94e3501aeacb8bda4712350cf3565249c0facab
Michael Lynch <mtlynch@google.com>
2017-04-04 20:23:30 -0400

    Adding an image indexer
    The backend is going to be placing images in the frontend's static files
    location, but the client-side JavaScript needs a way of knowing what files
    are there. The images indexer (and API) give the client-side JavaScript an
    index of these files.

---
+++ server/images.py
@@ -0,0 +1,59 @@
+import datetime
+import os
+
+import pytz
+
+# Format of GreenPiThumb image files.
+_FILENAME_FORMAT = '%Y-%m-%dT%H%MZ.jpg'
+
+
+def _files_in_directory(directory):
+    paths = os.listdir(directory)
+    return [f for f in paths if os.path.isfile(os.path.join(directory, f))]
+
+
+def _timestamp_from_filename(filename):
+    return datetime.datetime.strptime(filename, _FILENAME_FORMAT).replace(
+        tzinfo=pytz.utc)
+
+
+def _filename_to_index_entry(filename):
+    return {
+        'timestamp': _timestamp_from_filename(filename),
+        'filename': filename,
+    }
+
+
+class Indexer(object):
+    """Creates an index of GreenPiThumb image files."""
+
+    def __init__(self, image_path):
+        """Creates a new Indexer instance
+
+        Args:
+            image_path: Path to the GreenPiThumb images directory.
+        """
+        self._image_path = image_path
+
+    def index(self):
+        """Generates an index of the GreenPiThumb
+
+        Creates an index of all the GreenPiThumb image files in the specified
+        image path. If there are non-image files in the directory, these are
+        ignored. Any GreenPiThumb image files in subdirectories are also
+        ignored.
+
+        Returns:
+            A list of dictionaries, one for each GreenPiThumb image file, where
+            the dictionary has keys 'timestamp' with the datetime when the file
+            was created and 'filename' of the image's filename (without path
+            prefix).
+        """
+        file_index = []
+        for filename in _files_in_directory(self._image_path):
+            try:
+                file_index.append(_filename_to_index_entry(filename))
+            except ValueError:
+                # Ignore filenames that can't be parsed as GrenPiThumb images.
+                pass
+        return file_index
---
+++ tests/test_images.py
@@ -0,0 +1,87 @@
+import datetime
+import os
+import shutil
+import tempfile
+import unittest
+
+import pytz
+
+from server import images
+
+
+class ImagesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        self.indexer = images.Indexer(self.temp_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+
+    def test_indexes_image_files(self):
+        open(os.path.join(self.temp_dir, '2017-04-01T1851Z.jpg'), 'a').close()
+        open(os.path.join(self.temp_dir, '2017-04-01T1853Z.jpg'), 'a').close()
+        open(os.path.join(self.temp_dir, '2017-04-02T0000Z.jpg'), 'a').close()
+        self.assertItemsEqual(
+            [
+                {
+                    'timestamp': datetime.datetime(
+                        2017, 4, 1, 18, 51, 0, tzinfo=pytz.utc),
+                    'filename': '2017-04-01T1851Z.jpg',
+                },
+                {
+                    'timestamp': datetime.datetime(
+                        2017, 4, 1, 18, 53, 0, tzinfo=pytz.utc),
+                    'filename': '2017-04-01T1853Z.jpg',
+                },
+                {
+                    'timestamp': datetime.datetime(
+                        2017, 4, 2, 0, 0, 0, tzinfo=pytz.utc),
+                    'filename': '2017-04-02T0000Z.jpg',
+                },
+            ],
+            self.indexer.index())
+
+    def test_excludes_non_greenpithumb_image_files(self):
+        open(os.path.join(self.temp_dir, '2017-04-01T1851Z.jpg'), 'a').close()
+        open(os.path.join(self.temp_dir, '2017-04-01T1853Z.txt'), 'a').close()
+        open(os.path.join(self.temp_dir, 'dummyfile.jpg'), 'a').close()
+        open(
+            os.path.join(self.temp_dir, '2017-04-01T1845Z-extrajunk.jpg'),
+            'a').close()
+        self.assertItemsEqual(
+            [{
+                'timestamp': datetime.datetime(
+                    2017, 4, 1, 18, 51, 0, tzinfo=pytz.utc),
+                'filename': '2017-04-01T1851Z.jpg',
+            },],
+            self.indexer.index())
+
+    def test_excludes_directories(self):
+        open(os.path.join(self.temp_dir, '2017-04-01T1851Z.jpg'), 'a').close()
+        os.mkdir(os.path.join(self.temp_dir, 'dummydir'))
+        # Make a directory named like an image file.
+        os.makedirs(os.path.join(self.temp_dir, '2017-04-05T0000Z.jpg'))
+
+        self.assertItemsEqual(
+            [{
+                'timestamp': datetime.datetime(
+                    2017, 4, 1, 18, 51, 0, tzinfo=pytz.utc),
+                'filename': '2017-04-01T1851Z.jpg',
+            },],
+            self.indexer.index())
+
+    def test_excludes_subdirectories(self):
+        subdirectory_path = os.path.join(self.temp_dir, 'child_dir')
+        os.mkdir(subdirectory_path)
+        open(os.path.join(self.temp_dir, '2017-04-01T1851Z.jpg'), 'a').close()
+        open(os.path.join(subdirectory_path, '2017-05-08T0000Z.jpg'),
+             'a').close()
+
+        self.assertItemsEqual(
+            [{
+                'timestamp': datetime.datetime(
+                    2017, 4, 1, 18, 51, 0, tzinfo=pytz.utc),
+                'filename': '2017-04-01T1851Z.jpg',
+            },],
+            self.indexer.index())
